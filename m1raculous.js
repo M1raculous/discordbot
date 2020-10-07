be13b0915e1310d8c59c835bbf91bbfb02c5bbfb
M1raculous <63843137+M1raculous@users.noreply.github.com>
2020-10-07 15:01:10 +0200

    Update m1raculous.js

--- m1raculous.js
+++ m1raculous.js
@@ -1,5 +1,5 @@
-const Discord = require ('discord.js';) // It need to know that it's d.js.
-
+const {Discord, Client, Collection, MessageEmbed} = require("discord.js"); 
+const token = 'NotU1NDE5NDkxOTE1MR3AL.X2DBOQ.TOK-3.NGIiPdSvX4f_B1NyoDxd4p2Y' //https://discord.com/developers/applications/, create your own here :P
 /*
 So, this is just a template, i've made for you, therefore YOU SHALL NOT just drag & drop it. Since there is a lot commands that isn't uptimezed. 
 
@@ -13,7 +13,15 @@ I will show to make a simple command in your index, in this case i've called it 
 I will also show how to make a command in a other folder, this case it's commands...
 */
 
-client.on(ready)
-console.log('hello world');
+//when we wan't something to dissapear, i make it thru my main file. i do like this:
+client.on("message"), async mesage => {
 
-client.login(token); // logs in
+  msg = message.content.toLowerCase();
+
+    	if (message.author.bot) return;
+        if (msg.startsWith ("!test")) { // if the command is !test then it will
+        message.delete(); //delete the message.
+    }
+})
+
+client.login(token); // logs the bot, so it starts
