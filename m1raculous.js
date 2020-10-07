ca7cb01bef48096e95030181cd035fa0b18a5228
M1raculous <63843137+M1raculous@users.noreply.github.com>
2020-10-07 13:45:09 +0200

    Create m1raculous.js

---
+++ m1raculous.js
@@ -0,0 +1,19 @@
+const Discord = require ('discord.js';) // It need to know that it's d.js.
+
+/*
+So, this is just a template, i've made for you, therefore YOU SHALL NOT just drag & drop it. Since there is a lot commands that isn't uptimezed. 
+
+Please remember, that you can use this as much as you want, but please don't remove my watermarks, i still want some credit....
+
+also, make sure to add me on Discord. Either it's M1raculous#1234, M1raculous#0001, M1raculous#1337. I switch, but you can find my tags, in the code, i update it every week :D
+*/
+
+/*
+I will show to make a simple command in your index, in this case i've called it my name..
+I will also show how to make a command in a other folder, this case it's commands...
+*/
+
+client.on(ready)
+console.log('hello world');
+
+client.login(token); // logs in
